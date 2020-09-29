f8bbdc482438b32c4520147d8889dbd8025b391c
kimhakjun <dkd7142@naver.com>
2020-09-30 00:38:32 +0900

    register 미들웨어 작성 1. bcrpyt 활용하여 pw 암호화

--- back/src/controller/auth.js
+++ back/src/controller/auth.js
@@ -1,8 +1,25 @@
-const User = require('../models/user');
-
+const {User} = require('../models');
+const bcrypt = require('bcrypt');
+const createError = require('http-errors');
+const {isEmail, isPassword, isNick} = require('../util/validator');
 
 exports.register = async (req, res, next) => {
+    try{
+        const {email, password, nick} = req.body;
+        const checkUser = await User.findOne({where: {email}});
+        if(checkUser) return next(createError(400, 'already Existed'));
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+            email,
+            password: hash,
+            nick
+        });
+        res.status(200).json({success: true, message: 'register success'});
 
+    }catch(err){
+        console.error(err);
+        next(createError(500, 'server Error'));
+    }
 
 }
 
@@ -18,4 +35,14 @@ exports.kakaoCallBack = (req, res, next) => {
 exports.generateToken = (req, res, next) => {
     console.log('test');
     res.json({test:'test'});
+}
+
+exports.validateInputs = ({type}) => {
+    const checkType = type;
+    return (req, res, next)=>{
+        const {email, nick, password} = req.body;
+        if(checkType === 'register' && isEmail(email) && isPassword(password) && isNick(nick)) return next();
+        if(checkType === 'login' && isEmail(email) && isPassword(password)) return next(); 
+        return next(createError(400, 'invalid inputs'))
+    }
 }
--- back/src/route/auth.js
+++ back/src/route/auth.js
@@ -3,10 +3,13 @@ const passport = require('passport');
 const {
     register,
     kakaoLogin,
-    kakaoCallBack
+    kakaoCallBack,
+    validateInputs
 } = require('../controller/auth');
 
 const router = express.Router();
+
+router.post('/register', validateInputs({type: 'register'}), register);
 
 router.get('/kakao', passport.authenticate('kakao'));
 router.get('/kakao/callback', passport.authenticate('kakao', { failureRedirect: '/', }), kakaoCallBack);
