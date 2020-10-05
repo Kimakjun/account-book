c1e541f0cad76dbcb201152d6d76f7cfdc7b54a9
kimhakjun <dkd7142@naver.com>
2020-10-05 15:52:35 +0900

    webpack. babel 설정 1. sass loader, file loder, babel loader 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 
-
+import './index.scss';
 
 export class App {
 
     constructor(root){
-        
+        const div = document.createElement('div');
+        div.innerHTML = `<div>hello</div>`
+        root.appendChild(div);
     }
 
 }
