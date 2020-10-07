602e5e90a6bb37ddfbad253ee1381c87f3c683b1
kimhakjun <dkd7142@naver.com>
2020-10-08 01:46:59 +0900

    tranInput 구현 및 디자인. 1. tranInput 영역 구현, 기능 x

--- front/src/components/TransactionFrom.js
+++ front/src/components/TransactionFrom.js
@@ -1,13 +1,69 @@
+import { $new } from "../util/dom";
+import "../public/tranInput.scss";
+
 class TransactionForm {
   constructor({ root }) {
     this.root = root;
+    this.transactionInput = $new("div", "tranInput");
     this.init();
     this.render();
   }
 
-  init() {}
+  init() {
+    this.transactionInput.innerHTML = `
+        <div class="tranInput__firstSection">
+            <div class="tranInput__firstSection__clasify">
+                <span>분류</span>
+                <div class="tranInput__firstSection__clasify--isIncome">수입</div>
+                <div class="tranInput__firstSection__clasify--isExpenditure selected ">지출</div>
+            </div>
+            <div class="tranInput__firstSection__update">
+                <button class="tranInput__firstSection__update--delete">내용 지우기</button>
+            </div>
+        </div>
+        <div class="tranInputContainer__secondSection">
+            <div class="tranInputContainer__secondSection__date">
+                <span>날짜</span>
+                <input type="date"/>
+            </div>
+            <div class="tranInputContainer__secondSection__date">
+                <span>카테고리</span>
+                <select name="category">
+                    <option value="외식">외식</option>
+                    <option value="뷰티">뷰티</option>
+                    <option value="생활" selected="selected">생활</option>
+                    <option value="기타">기타</option>
+                </select>
+            </div>
+            <div class="tranInputContainer__secondSection__date">
+                <span>결제수단</span>
+                <select name="category">
+                    <option value="외식">외식</option>
+                    <option value="뷰티">뷰티</option>
+                    <option value="생활" selected="selected">생활</option>
+                    <option value="기타">기타</option>
+                </select>
+            </div>
+        </div>
+        <div class="tranInputContainer__thirdSection">
+            <div class="tranInput__firstSection__clasify">
+                <span>금액</span>
+                <input/>
+            </div>
+            <div class="tranInput__firstSection__clasify">
+                <span>내용</span>
+                <input/>
+            </div>
+        </div>
+        <div class="tranInputContainer__lastSection--button">
+            확인
+        </div>
+    `;
+  }
 
-  render() {}
+  render() {
+    this.root.appendChild(this.transactionInput);
+  }
 }
 
 export default TransactionForm;
--- front/src/views/MainPage.js
+++ front/src/views/MainPage.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import NabVar from "../components/Navbar";
+import TranInput from "../components/TransactionFrom";
 
 class MainPage {
   constructor({ root }) {
     new Header({ root });
     new NabVar({ root });
+    new TranInput({ root });
     // 가계부 => 입력영역, 조회역영
     // 달력
     // 날짜통계표
